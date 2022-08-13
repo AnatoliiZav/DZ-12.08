cd7f799d46c36079d15e53efba103d14d3ae6383
Anatolii <zavorinanatolii@mail.ru>
2022-08-13 20:59:21 +0800

    Задание 19

---
+++ Dz-19/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Console.WriteLine("Введите пятизначное число : ");
+int N = int.Parse(Console.ReadLine());
+
+if (N > 9999 && N <100000) 
+{
+    int a = N % 100; 
+    int b = N / 1000; 
+    int a1 = a / 10; 
+    int b1 = a % 10; 
+    int b2 = b / 10; 
+    int a2 = b % 10;
+    Console.WriteLine($"{a}, {b}, {a1}, {a2}, {b1}, {b2}");
+    if(a1 == a2 && b1 == b2)
+    { 
+        Console.Write($"Число {N} является палиндромом");
+    }
+    else
+    {
+        Console.Write($"Число {N} не является палиндромом");
+    }
+}
+else
+{
+    Console.Write($"Число {N} не является пятизначным");
+}
